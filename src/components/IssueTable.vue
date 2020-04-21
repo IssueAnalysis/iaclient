b1d34a7a724e9489b25fe69fbc7b305674983858
张宇杰 <845075906@qq.com>
2020-04-21 18:29:40 +0800

    feat: add collect and check info func

--- src/components/IssueTable.vue
+++ src/components/IssueTable.vue
@@ -21,6 +21,15 @@
                 <template v-slot:item.description="{ item }">
                     <div v-html="item.description" v-highlight></div>
                 </template>
+                <template v-slot:item.actions="{ item }">
+                    <v-btn color="rgba(255, 0, 0, 0)" @click="check(item)">
+                        <img :src=checkIcon style="width:20px;height:20px"/>
+                    </v-btn>
+
+                    <v-btn color="rgba(255, 0, 0, 0)" @click="collect(item)">
+                        <img :src=collectIcons[item.collect?1:0] style="width:20px;height:20px"/>
+                    </v-btn>
+                </template>
                 <template v-slot:no-data>
                     <v-btn color="primary" @click="initialize">Reset</v-btn>
                 </template>
@@ -29,7 +38,10 @@
 </template>
 
 <script>
-    // import axios from 'axios'
+    import axios from 'axios'
+    import collectIcon from '../assets/collect.png'
+    import noCollectIcon from '../assets/noCollect.png'
+    import checkIcon from '../assets/check.png'
     export default {
         name: "issueTable",
         data: () => ({
@@ -41,13 +53,17 @@
                 { text: '详细描述', value: 'description' },
                 { text: '关注点', value: 'intention' },
                 { text: '考虑', value: 'consideration' },
+                { text: '', value: 'actions', sortable: false },
             ],
+            collectIcons:[collectIcon,noCollectIcon],
+            checkIcon: checkIcon,
             issues: [{
                 id:"1",
                 summary:"testSummary",
                 description:"这是一段代码<pre><code>int main(){ print(\"hello world!\");}</code></pre>",
                 intention:"testIntention",
                 consideration:"testConsideration",
+                collect:false
             },
                 {
                     id:"2",
@@ -55,6 +71,7 @@
                     description:"这是一段代码<pre><code>int main(){ print(\"hello world!\");}</code></pre>",
                     intention:"testIntention",
                     consideration:"testConsideration",
+                    collect:false
                 }],
         }),
         props:{
@@ -88,6 +105,26 @@
                 //     })
                 console.log("init  "+this.actionType)
             },
+            check(item){
+                this.$route.push('/issue_info',item.id)
+            },
+            collect(item){
+                const app = this
+                const index = app.issues.indexOf(item)
+                let msg = "收藏失败, 请稍后重试"
+                axios.post('/api/action/collect_issues',{
+                    issues: [item.id]
+                }).then(res=>{
+                    if(res.data.ret){
+                        app.issues[index].collect = !app.issues[index].collect
+                        msg = "收藏成功"
+                        app.$message.success(msg)
+                    }
+                    app.$message.error(msg)
+                }).catch(()=>{
+                    app.$message.error(msg)
+                })
+            }
         }
     }
 </script>
